a0d10ca23c14c5b80b1b64eeff6758c228afe32c
Selcuk Pinarbasi <selcukpinar@gmail.com>
2026-02-09 17:17:27 +0300

    fix(incident-service): disable forbidNonWhitelisted to allow extra query params

--- services/incident-service/src/main.ts
+++ services/incident-service/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     // Global validation pipe
     app.useGlobalPipes(new ValidationPipe({
         whitelist: true,
-        forbidNonWhitelisted: true,
+        forbidNonWhitelisted: false,
         transform: true,
     }));
 
